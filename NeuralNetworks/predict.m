cc6d75ae41234676198d6197b3daffbb3a812714
sreejithsreekantan <krssreejith@gmail.com>
2014-10-27 17:24:01 +0530

    Update predict.m
    Neural Network feed-forward-propagation algorithm

--- NeuralNetworks/predict.m
+++ NeuralNetworks/predict.m
@@ -21,12 +21,13 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
+X = [ones(m,1) X];
 
+a1 = sigmoid(X*Theta1');
+a1 = [ones(m,1) a1];
+a2 = sigmoid(a1*Theta2');
 
-
-
-
-
+[val p] = max(a2, [], 2);
 
 
 % =========================================================================
